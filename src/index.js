89701ed9892789ea969b3bd6aa06f98db6596567
andersentess <andersentess@gmail.com>
2023-11-24 22:34:28 -0700

    added ai api integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-function generateFact(event) {
-  event.preventDefault();
-
+function displayFact(response) {
+  console.log("Fact generated");
   new Typewriter("#fact", {
-    strings: "here is your random fact",
+    strings: response.data.answer,
     autoStart: true,
     delay: 40,
     cursor: "",
   });
+}
+
+function generateFact(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "0ob8f6fd5f71ctf4bb659c63840b18fa";
+  let prompt = `User instructions: Generate a random fact about ${instructionsInput.value}`;
+  let context =
+    "You are random fact expert and love to share your knowledge with everyone. Your mission is to generate a short random fact in basic HTML. Make sure to follow the user instructions. Do not include a title to the Fact. Sign the fact with 'SheCodes AI' inside a <strong> element at the end of the fact and NOT at the beginning";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating fact");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayFact);
 }
 
 let randomFactFormElement = document.querySelector("#fact-generator-form");
